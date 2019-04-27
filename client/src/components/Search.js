78062c720a15b54a491c18f805b16db8e0f7ac72
Anthony Ham <xnthiny@gmail.com>
2019-04-27 18:35:55 -0400

    added foursquare api and retrieved data

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
+import API from "../utils/API"
 
-export default function Search() {
-  return (
-    <div>
-      <h1>Search Component</h1>
-    </div>
-  )
+export default class Search extends Component {
+  state = {
+    results:{},
+    inputValue: ""
+  } 
+
+  handleSearch = () => {
+    API.searchFoursquare(this.state.inputValue).then(res => {
+      this.setState({results: res.data.response.venues})
+      console.log(this.state.results)
+    })
+  }
+
+  updateInputValue = (e) => {
+    this.setState({inputValue: e.target.value})
+  }
+
+  render() {
+    return (
+      <div>
+        <input value={this.state.inputValue} onChange={this.updateInputValue}></input>
+        <button onClick={this.handleSearch}>Search</button>
+      </div>
+    )
+  }
 }
