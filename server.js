1a48e55ea697937da302ad12022facdb9fb864f5
Marlon Villegas <marlonvillegas@Marlons-MacBook-Air.local>
2019-05-04 14:29:30 -0400

    added db

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const routes = require("./drinkery/routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const dbRoute= "mongodb://127.0.0.1:27017";
+
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -16,7 +18,55 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(
+  dbRoute,
+  { useNewUrlParser: true }
+);
+
+let db = mongoose.connection;
+
+db.once("open", () => console.log("connected to the database"));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+router.get("/getData", (req, res) => {
+  Data.find((err, data) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true, data: data });
+  });
+});
+router.post("/updateData", (req, res) => {
+  const { id, update } = req.body;
+  Data.findOneAndUpdate(id, update, err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+router.delete("/deleteData", (req, res) => {
+  const { id } = req.body;
+  Data.findOneAndDelete(id, err => {
+    if (err) return res.send(err);
+    return res.json({ success: true });
+  });
+});
+router.post("/putData", (req, res) => {
+  let data = new Data();
+
+  const { id, message } = req.body;
+
+  if ((!id && id !== 0) || !message) {
+    return res.json({
+      success: false,
+      error: "INVALID INPUTS"
+    });
+  }
+  data.message = message;
+  data.id = id;
+  data.save(err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+app.use("/api", router);
+app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${PORT}`));
 
 // Start the API server
 app.listen(PORT, function() {
