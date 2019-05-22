8e70272cfcbd8234ed255c9fbc159dad9bb69405
marcobo <marcobrugo@mac.com>
2019-05-21 21:36:38 -0400

    pushing to branch and redownloading packalock and modules

--- client/src/components/AgeVerification/VerifyAge.js
+++ client/src/components/AgeVerification/VerifyAge.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import backdrop from '../Navbar/Backdrop/Backdrop';
 import bg from '../../images/bg.png'
+import "./VerifyAge.css"
 
 let styles = {
     container: {
@@ -8,7 +9,7 @@ let styles = {
     }
 }
 
-export default function VerifyAge(props) {
+export default function VerifyAge() {
     return (
         <div className='verify-background-container' style={styles.container}>
             <img id='verify-background-image' src={bg} />
